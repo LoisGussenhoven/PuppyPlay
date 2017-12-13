821d58cfa7991fec27e7bea6ae781e483b938798
Lois Gussenhoven <lois_gussenhoven@hotmail.nl>
2017-12-13 17:11:21 +0100

    from mainactivity to create after 2 seconds

--- app/src/main/java/com/example/loisgussenhoven/puppyplay/MainActivity.java
+++ app/src/main/java/com/example/loisgussenhoven/puppyplay/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,7 +14,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent i = new Intent(getApplicationContext(), CreateActivity.class);
-        startActivity(i);
+        new Timer().schedule(new TimerTask() {
+            public void run() {
+                startActivity(new Intent(MainActivity.this, CreateActivity.class));
+            }
+        }, 2000);
     }
 }
